6cec552a908a2fef02a58ff676d0dd11c34b7aff
Darwin Quiñones <darwinquinones735@gmail.com>
2022-10-27 03:36:45 -0500

    se cambia la url de la api2

--- src/components/Car/CarServer.jsx
+++ src/components/Car/CarServer.jsx
@@ -1,12 +1,13 @@
-const API_URL_AUTOMOVILES = 'https://crud-automvil-php.000webhostapp.com/index.php'
+// const API_URL_AUTOMOVILES = 'https://crud-automvil-php.000webhostapp.com/index.php'
+const API_URL_AUTOMOVILES = 'http://localhost/crud_automovil/'
 
 export const CarList = async () =>{
     var cars = await fetch(API_URL_AUTOMOVILES, {
-        method: 'GET',
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            // 'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8;application/json'
-        }
+        // method: 'GET',
+        // headers: {
+        //     'Access-Control-Allow-Origin': '*',
+        //     // 'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8;application/json'
+        // }
     })
     return cars
 }
